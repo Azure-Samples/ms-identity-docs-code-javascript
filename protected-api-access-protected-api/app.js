19e98443ee226204f0e284a76d49ff985536f808
Jason Bouska <82831332+skabou@users.noreply.github.com>
2021-12-29 13:56:33 -0800

    Update app.js

--- protected-api-access-protected-api/app.js
+++ protected-api-access-protected-api/app.js
@@ -1,25 +1,53 @@
+/*
+ This application demonstrates how to issue a call to a protected API endpoint
+ using the On-behalf-of flow.  This application will issue a request to
+ the Microsoft Graph /me endpoint on behalf of the authenticated user.
+*/
+
+// Microsoft Authentication Library (MSAL) for Node.Js
 const msal = require('@azure/msal-node')
+
+// Node.Js Express Framework
 const express = require('express')
+
+// Used to make HTTP GET requests to Graph
 const https = require('https')
+
+// Used to verify tokens
 const jsonwebtoken = require('jsonwebtoken')
 const jwksRsa = require('jwks-rsa')
 
+// The port the express server will listen on
 const SERVER_PORT = 8080
 
+// 'Directory (tenant) ID' in Azure portal - this value is a GUID
 const tenant = ''
 
 const config = {
   auth: {
+    
+    // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
     clientId: '',
+    
+    // Client secret 'Value' (not its ID) from 'Client secrets' in app registration in Azure portal
     clientSecret: '',
+
+    // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
     authority: `https://login.microsoftonline.com/${tenant}`
   }
 }
 
+// Initialize MSAL configuration with above values
 const cca = new msal.ConfidentialClientApplication(config)
 
 const app = express()
 
+// Needed to validate tokens
+const client = jwksRsa({
+  jwksUri: `https://login.microsoftonline.com/${tenant}/discovery/v2.0/keys`
+})
+
+// Attempt to validate the token. If validation fails, present the user with an HTTP 401 error.
 const validateJwt = (req, res, next) => {
   const authHeader = req.headers.authorization
 
@@ -42,16 +70,13 @@ const validateJwt = (req, res, next) => {
 }
 
 const getSigningKeys = (header, callback) => {
-  const client = jwksRsa({
-    jwksUri: `https://login.microsoftonline.com/${tenant}/discovery/v2.0/keys`
-  })
-
   client.getSigningKey(header.kid, function (err, key) {
     const signingKey = key.publicKey || key.rsaPublicKey
     callback(null, signingKey)
   })
 }
 
+// This portion responds to the user when the /me endpoint is requested
 app.get('/me', validateJwt, (req, res) => {
   const authHeader = req.headers.authorization
 
@@ -60,6 +85,7 @@ app.get('/me', validateJwt, (req, res) => {
     scopes: ['user.read']
   }
 
+  // Request a new token On-behalf-of the user, using the token provided by the user when they accessed the /me endpoint
   cca.acquireTokenOnBehalfOf(oboRequest).then((response) => {
     const options = {
       method: 'GET',
