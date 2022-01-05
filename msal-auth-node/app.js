4458c88e41a1c3211f640e46774a8566b7fe6fbc
Jason Bouska <82831332+skabou@users.noreply.github.com>
2022-01-05 15:19:05 -0800

    Update app.js

--- msal-auth-node/app.js
+++ msal-auth-node/app.js
@@ -1,87 +1,38 @@
 /*
- This application demonstrates how to include authorization
- to an API written in Node.Js 16.
+ This application demonstrates how to include
+ authorization for an API endpoint in Node.Js 16.
 */
 
 // Node.Js Express Framework
 const express = require('express')
 
-// Used to validate authentication tokens
-const jsonwebtoken = require('jsonwebtoken')
-const jwksRsa = require('jwks-rsa')
-
-// The port the express server will listen on
-const SERVER_PORT = 8080
-
-// 'Directory (tenant) ID' in the Azure portal
-const tenant = ''
-
-const config = {
-  auth: {
-
-    // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
-    clientId: '',
-
-    // Full directory URL, in the form of https://login.microsoftonline.com/<tenant>
-    authority: `https://login.microsoftonline.com/${tenant}`
-  }
-}
-
-// Scopes required for authentication
-const requiredScopes = ['user_write', 'user_read']
+// Libraries used to validate authentication tokens
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+const jwtAuthz = require('express-jwt-authz')
 
 const app = express()
 
-// The signing keys are located on the specified JWKS (JSON Web Key Set) endpoint
-// Required for token validation
-const client = jwksRsa({
-  jwksUri: `https://login.microsoftonline.com/${tenant}/discovery/v2.0/keys`
+app.use(jwt({
+   secret: jwks.expressJwtSecret({
+   
+   // Full URL, in the form of: https://login.microsoftonline.com/<tenant>/discovery/v2.0/keys
+   jwksUri: ''
+  }),
+ 
+  // 'Application (client) ID' of app registration in the Azure portal - this value is a GUID
+  audience: '',
+ 
+  // Full URL, in the form of: https://login.microsoftonline.com/<tenant>/v2.0
+  issuer: '',
+ 
+  // Encryption algorithm
+  algorithms: ['RS256']
+}))
+
+// Verify the token is valid and contains 'Greeting.Read' for the scope to access the / endpoint
+app.get('/', jwtAuthz(['Greeting.Read'], { customScopeKey: 'scp' }), (req, res) => {
+  res.send('Hello, world. You were able to access this because you provided a valid access token with the Greeting.Read scope as a claim.')
 })
 
-const validateJwt = (req, res, next) => {
-  const authHeader = req.headers.authorization
-
-  if (authHeader) {
-
-    // Extract the authorization tokenfromthe header
-    const token = authHeader.split(' ')[1]
-
-    const validationOptions = {
-      // The expected audience is the clientId specified in 'audience'
-      audience: config.auth.clientId,
-      // The expected issuer is the authority specified in 'config'
-      issuer: config.auth.authority + '/v2.0'
-    }
-
-    // Verify the token - 'expiry date' and 'not before date' are verified by default
-    // Per the variable validationOptions, 'audience' and 'issuer' will also be verified
-    jsonwebtoken.verify(token, getSigningKeys, validationOptions, (err, payload) => {
-      if (err) {
-        console.log(err)
-        return res.sendStatus(401)
-      }
-
-      // If the token is valid, verify the user has the correct scope(s) to access the desired endpoint
-      if (!requiredScopes.every(v => payload.scp.split(' ').includes(v))) {
-        console.log('User does not have required scope(s)')
-        return res.sendStatus(403)
-      }
-
-      next()
-    })
-  }
-}
-
-// Get the public key used to sign the token
-const getSigningKeys = (header, callback) => {
-  client.getSigningKey(header.kid, function (err, key) {
-    callback(null, key.publicKey)
-  })
-}
-
-// Endpoint which displays 'Hello world' upon successful authentication
-app.get('/', validateJwt, (req, res) => {
-  res.status(200).send('Hello world')
-})
-
-app.listen(SERVER_PORT, () => console.log(`\nListening here:\nhttp://localhost:${SERVER_PORT}/`))
+app.listen(8080, () => console.log('\nListening here:\nhttp://localhost:8080'))
