32e70e98e73a01e097c1200f33a054307227fd62
Jason Bouska <82831332+skabou@users.noreply.github.com>
2022-01-14 14:40:55 -0800

    Initial import of "Protect an API" scenario - Node.js (#4)
    * Create app.js
    
    * Create package.json
    
    * Create package-lock.json
    
    * Create README.md
    
    * Update README.md
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update app.js
    
    * Update app.js
    
    * Update package.json
    
    * Update package-lock.json
    
    * Update README.md
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/app.js
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/app.js
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update app.js
    
    Updated code to be more in alignment with other Node task
    
    * Update README.md
    
    Updating with lessons learned
    
    * Update README.md
    
    * Update README.md
    
    removed irrelevant json stuff (graph output)
    
    * Update app.js
    
    * Update README.md
    
    * Apply suggestions from code review
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    
    * Update app.js
    
    * Update README.md
    
    * Update README.md
    
    * Update msal-auth-node/README.md
    
    Co-authored-by: Marsh Macy <mmacy@users.noreply.github.com>
    
    * Update README.md
    
    * Update README.md
    
    Removed 'web'
    
    * Update README.md
    
    removed MSAL, reinstated 'web'
    
    * Update README.md
    
    StackOverflow: -msal +express-jwt
    
    * Rename msal-auth-node/README.md to protect-api/README.md
    
    * Rename msal-auth-node/app.js to protect-api/app.js
    
    * Rename msal-auth-node/package.json to protect-api/package.json
    
    * Rename msal-auth-node/package-lock.json to protect-api/package-lock.json
    
    * Update README.md
    
    Co-authored-by: Chad Kittel <chad.kittel@gmail.com>
    Co-authored-by: Marsh Macy <mmacy@users.noreply.github.com>

---
+++ protect-api/app.js
@@ -0,0 +1,44 @@
+/*
+ This application demonstrates how to include
+ authorization for an API endpoint in Node.js 16.
+*/
+
+
+// Node.js Express Framework
+const express = require('express')
+
+// Used to validate JWT access tokens
+const jwt = require('express-jwt')
+const jwks = require('jwks-rsa')
+const jwtAuthz = require('express-jwt-authz')
+
+const config = {
+  auth: {
+    // 'Directory (tenant) ID' of app registration in the Azure portal - this value is a GUID
+    tenant: '',
+
+    // 'Application (client) ID' of app registration in the Azure portal - this value is a GUID
+    audience: ''
+  }
+}
+
+// Initialize Express
+const app = express()
+
+// Add Express middleware to validate JWT access tokens
+app.use(jwt({
+  secret: jwks.expressJwtSecret({
+    jwksUri: 'https://login.microsoftonline.com/' + config.auth.tenant + '/discovery/v2.0/keys'
+  }),
+  audience: config.auth.audience,
+  issuer: 'https://login.microsoftonline.com/' + config.auth.tenant + '/v2.0',
+  algorithms: ['RS256']
+}))
+
+// Verify the JWT access token is valid and contains 'Greeting.Read' for the scope to access the endpoint.
+// Instruct jwtAuthz to pull scopes from the 'scp' claim, which is the claim used by Azure AD.
+app.get('/', jwtAuthz(['Greeting.Read'], { customScopeKey: 'scp' }), (req, res) => {
+  res.send('Hello, world. You were able to access this because you provided a valid access token with the Greeting.Read scope as a claim.')
+})
+
+app.listen(8080, () => console.log('\nListening here:\nhttp://localhost:8080'))
