66a2b80088a19873ae11e3550ccf49e072393087
Jason Bouska <82831332+skabou@users.noreply.github.com>
2022-01-26 13:27:16 -0800

    After running 'Prettier'
    After running 'Prettier'

--- protect-function-app/greeting/index.js
+++ protect-function-app/greeting/index.js
@@ -6,19 +6,21 @@
 */
 
 // This module decodes JSON web tokens
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports = async function (context, req) {
   try {
     // Decode the JSON web token which is passed in via the Authorization header
-    const decodedToken = jwt.decode(req.headers.authorization.split(' ')[1])
+    const decodedToken = jwt.decode(req.headers.authorization.split(" ")[1]);
 
     // If the scope includes 'Greeting.Read', then accept the request with a greeting message and default 200 status code
     // Otherwise reject the request with a failure message and 403 status code
-    context.res = (decodedToken.scp.includes('Greeting.Read'))
-      ? { body: 'Hello, world. You were able to access this because you provided a valid access token with the Greeting.Read scope as a claim.' }
-      : { body: 'Missing required scope.', status: 403 }
-  } catch(error) {
-    context.res = { body: 'Invalid token.', status: 403 }
+    context.res = decodedToken.scp.includes("Greeting.Read")
+      ? {
+          body: "Hello, world. You were able to access this because you provided a valid access token with the Greeting.Read scope as a claim.",
+        }
+      : { body: "Missing required scope.", status: 403 };
+  } catch (error) {
+    context.res = { body: "Invalid token.", status: 403 };
   }
-}
+};
