ddd69e590f37c578e645fc10a272a75f208a37c6
Dickson-Mwendia <dicksonmwendia99@gmail.com>
2022-01-26 07:05:15 +0300

    Add React single-page sample app

---
+++ react-single-page-app/README.md
@@ -0,0 +1,31 @@
+# React single-page app (SPA) | Sign in users, call protected API | Microsoft identity platform
+
+This minimal React application demonstrates usage of the Microsoft Authentication Library for React (MSAL React) to:
+
+- Sign in Azure AD users (authentication)
+- Call a protected web API (authorization)
+- Sign out users
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
---
+++ react-single-page-app/src/index.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// <ms_docref_import_modules>
+import { Configuration,  PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider, useMsal, useIsAuthenticated } from "@azure/msal-react";
+// </ms_docref_import_modules>
+
+// <ms_docref_configure_msal>
+const configuration: Configuration = {
+  auth: {
+      clientId: "034ff080-d093-433d-915c-4f12ae03f100"
+  }
+};
+
+const clientInstance = new PublicClientApplication(configuration);
+// </ms_docref_configure_msal>
+
+// <ms_docref_get_graph_token>
+const getGraphToken =  async (msalInstance, accounts) => {
+  const tokenRequest = {
+    scopes: ["User.Read", "openid", "profile"],
+    account: accounts
+  }
+  
+  try{
+    const {accessToken} = await msalInstance.acquireTokenSilent(tokenRequest);
+    return accessToken;
+  } catch (e) {
+    const {accessToken} = await msalInstance.acquireTokenPopup(tokenRequest);
+    return accessToken;
+  }
+  
+}
+// </ms_docref_get_graph_token>
+
+// <ms_docref_make_graph_call>
+const MICROSOFT_GRAPH_URL = "https://graph.microsoft.com/v1.0"
+
+const fetchUserDetails = async (msalInstance, accounts, setUserDetails) => {
+  const bearer = `Bearer ${await getGraphToken(msalInstance, accounts)}`;
+  const response = await fetch(`${MICROSOFT_GRAPH_URL}/me`, { 
+    method: 'get', 
+    headers: new Headers({
+      'Authorization': bearer
+    })
+  }).then(res => res.json());
+
+  setUserDetails(response)
+}
+// </ms_docref_make_graph_call>
+
+const PageContent = () => {
+
+  const [userDetails, setUserDetails] = useState({});
+
+  // <ms_docref_use_msal_context>
+  const { instance, accounts } = useMsal();
+  // </ms_docref_use_msal_context>
+
+  const {name} = accounts[0]||{};
+
+  // <ms_docref_check_logged_in_state>
+  const isAuthenticated = useIsAuthenticated();
+
+  return isAuthenticated ? (
+    <div>You are logged in. Welcome {name}.<br/>
+      <button onClick={() => instance.logout()}>Logout</button>
+
+      <button onClick={() => fetchUserDetails(instance, accounts, setUserDetails)}>Fetch User Details from Graph</button>
+
+      {userDetails.displayName ? (
+        <div>
+          Display Name and Email
+          <br/>
+          <span> {userDetails.displayName} </span>
+          <span> {userDetails.mail} </span>
+        </div>
+        ): ""}
+
+    </div>
+    ):
+    (
+
+      // <ms_docref_add_login_button>
+      <div><button onClick={() => instance.loginPopup()}>Login</button> </div>
+      // </ms_docref_add_login_button>
+
+      );
+  // </ms_docref_check_logged_in_state>
+
+}
+
+// <ms_docref_use_msal_provider>
+ReactDOM.render(
+  <React.StrictMode>
+
+    <MsalProvider instance={clientInstance}>
+      <PageContent />
+    </MsalProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// </ms_docref_use_msal_provider>
