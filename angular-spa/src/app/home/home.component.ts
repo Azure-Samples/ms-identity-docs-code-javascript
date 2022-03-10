85f1f10712acd0de2a6b82f2fe8b04d8e0973e92
Jason Bouska <82831332+skabou@users.noreply.github.com>
2022-03-10 13:02:02 -0500

    Add files via upload

--- angular-spa/src/app/home/home.component.ts
+++ angular-spa/src/app/home/home.component.ts
@@ -1,6 +1,13 @@
+// Required for Angular
 import { Component, OnInit } from '@angular/core';
+
+// Required for MSAL
 import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+
+// Required for Angular multi-browser support
 import { EventMessage, EventType, AuthenticationResult } from '@azure/msal-browser';
+
+// Required for RJXS observables
 import { filter } from 'rxjs/operators';
 
 @Component({
@@ -8,10 +15,13 @@ import { filter } from 'rxjs/operators';
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
-  loginDisplay = false;
+  constructor(
+    private authService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) { }
 
-  constructor(private authService: MsalService, private msalBroadcastService: MsalBroadcastService) { }
-
+  // Subscribe to the msalSubject$ observable on the msalBroadcastService
+  // This allows the app to consume emitted events from MSAL
   ngOnInit(): void {
     this.msalBroadcastService.msalSubject$
       .pipe(
@@ -21,12 +31,5 @@ export class HomeComponent implements OnInit {
         const payload = result.payload as AuthenticationResult;
         this.authService.instance.setActiveAccount(payload.account);
       });
-
-      this.setLoginDisplay();
   }
-
-  setLoginDisplay() {
-    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
-  }
-
 }
