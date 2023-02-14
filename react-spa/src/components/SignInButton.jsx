d45e9338629370b8e722448cafe76460c39ea8dd
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2023-02-14 09:03:04 +0300

    Update SignInButton.jsx

--- react-spa/src/components/SignInButton.jsx
+++ react-spa/src/components/SignInButton.jsx
@@ -1,3 +1,5 @@
+// <ms_doc_ref_signin_button>
+
 import React from "react";
 import { useMsal } from "@azure/msal-react";
 import { loginRequest } from "../authConfig";
@@ -28,3 +30,5 @@ export const SignInButton = () => {
         </DropdownButton>
     )
 }
+
+// </ms_doc_ref_signin_button>
