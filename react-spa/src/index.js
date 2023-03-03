bfa32517a1771ce806a3606df6dec4592ce77a24
Henry Mbugua <82150047+henrymbuguakiarie@users.noreply.github.com>
2023-03-03 16:47:59 +0300

    Update index.js (#64)

--- react-spa/src/index.js
+++ react-spa/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-// <ms_docref_import_modules>
 import { PublicClientApplication } from '@azure/msal-browser';
 import { MsalProvider } from '@azure/msal-react';
 import { msalConfig } from './authConfig';
@@ -11,15 +10,10 @@ import { msalConfig } from './authConfig';
 // Bootstrap components
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// </ms_docref_import_modules>
-
-// <ms_docref_configure_msal>
 const msalInstance = new PublicClientApplication(msalConfig);
-// </ms_docref_configure_msal>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// <ms_docref_use_msal_provider>
 /**
  * We recommend wrapping most or all of your components in the MsalProvider component. It's best to render the MsalProvider as close to the root as possible.
  */
@@ -30,5 +24,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
       </MsalProvider>
   </React.StrictMode>
 );
-
-// </ms_docref_use_msal_provider>
